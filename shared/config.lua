68493cc69186dd5a0e9a4c3618b3c95b1a7a89bb
Xander Harrison <xander.harrison77@gmail.com>
2023-01-20 03:22:35 -0500

    Updating Locale and Configs

--- shared/config.lua
+++ shared/config.lua
@@ -1,1 +1,13 @@
-CoreConfig = exports.stax_configs:GetConfig("core")
+CoreConfig = exports.stax_configs:AddConfig("core", {
+    Identifier = "license",
+    Locale = "en",
+    Whitelisted = false,
+    DisableAI = false,
+    DisableWanted = true,
+    EnablePVP = true,
+    DebugEnabled = true,
+    DatabaseResources = {
+      "stax_core"
+    },
+    DefaultRole = {}
+})
--- shared/locale.lua
+++ shared/locale.lua
@@ -1,1 +1,10 @@
-CoreLocale = exports.stax_locales:GetLocale("core")
+CoreLocale = exports.stax_locales:AddLocale("core", {
+    ["en"] = {
+        ["connecting_retrieving_user_data"] = "Retrieving User Data!",
+        ["connecting_server_not_ready"] = "Server isn't ready to be connected to yet!",
+        ["connecting_welcome_back"] = "Welcome Back!",
+        ["connecting_not_whitelisted"] = "Sorry :( You are not whitelisted!",
+        ["connecting_creating_user"] = "Welcome! Creating your user data now!",
+        ["connecting_created_user"] = "Created Your User!"
+      }
+})
