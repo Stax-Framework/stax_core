a845c8e6ffe91af1646d95f8e4ddf6668ccd52f7
Xander Harrison <xander.harrison77@gmail.com>
2023-01-23 23:51:49 -0500

    Stopping client code temporarily

--- client/client.lua
+++ client/client.lua
@@ -7,29 +7,31 @@ end)
 Citizen.CreateThread(function()
   local player = PlayerId()
 
-  while true do
+  -- COMMENTING OUT UNTIL CLIENT SIDE PLUGINS ARE SET
+  -- while true do
 
-    SetPlayerHealthRechargeLimit(player, 0.0)
+  --   SetPlayerHealthRechargeLimit(player, 0.0)
 
-    if not CoreConfig.EnableAI then
-      DisableAI()
-    end
+  --   if not CoreConfig.EnableAI then
+  --     DisableAI()
+  --   end
 
-    if CoreConfig.DisabledWanted then
-      DisableWantedLevel(player)
-    end
+  --   if CoreConfig.DisabledWanted then
+  --     DisableWantedLevel(player)
+  --   end
 
-    Citizen.Wait(0)
-  end
+  --   Citizen.Wait(0)
+  -- end
 end)
 
 function FirstSpawn()
   if firstSpawn then
     SetNuiFocus(false, false)
 
-    if CoreConfig.EnablePVP then
-      EnablePVP()
-    end
+      -- COMMENTING OUT UNTIL CLIENT SIDE PLUGINS ARE SET
+    -- if CoreConfig.EnablePVP then
+    --   EnablePVP()
+    -- end
 
     exports.spawnmanager:spawnPlayer()
     exports.spawnmanager:setAutoSpawn(false)
