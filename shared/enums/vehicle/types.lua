c3f0f0283e9faf7bdf3bbe439c64b5b2630fabc5
Xander Harrison <xander.harrison77@gmail.com>
2023-01-25 20:40:48 -0500

    Adding Vehicle Types Enum

---
+++ shared/enums/vehicle/types.lua
@@ -0,0 +1,11 @@
+---@enum StaxVehicleTypes
+StaxVehicleTypes = {
+  automobile = "automobile",
+  bike = "bike",
+  boat = "boat",
+  heli = "heli",
+  plane = "plane",
+  submarine = "submarine",
+  trailer = "trailer",
+  train = "train"
+}
