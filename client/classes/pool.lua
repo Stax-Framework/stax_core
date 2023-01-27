bc5cf2ab8b156b2d6db3152023146e518acc1af1
Xander Harrison <xander.harrison77@gmail.com>
2023-01-27 05:51:08 -0500

    GamePool class to handle GetGamePool native

--- client/classes/pool.lua
+++ client/classes/pool.lua
@@ -1,21 +1,58 @@
+---@class Array<T>: { [integer]: T }
+
 ---@class StaxGamePool
 ---@field private Pool table Table that stores the game pool data
 StaxGamePool = {}
 StaxGamePool.__index = StaxGamePool
 
 --- Creates a new instance of StaxGamePool
----@param pool StaxPoolTypes The pool type you are trying to access
----@generic T
----@return StaxGamePool
-function StaxGamePool.New(pool)
+---@param poolType StaxPoolTypes
+---@return StaxGamePool | nil
+function StaxGamePool.New(poolType)
   local newPool = {}
-  setmetatable(newPool)
+  setmetatable(newPool, StaxGamePool)
   
+  local entities = GetGamePool(poolType)
+  if #entities <= 0 then
+    return nil
+  end
+
+  newPool.Type = poolType
   newPool.Pool = {}
+
+  if poolType == StaxPoolTypes.CVehicle then
+    for a = 1, #entities do
+      newPool.Pool[entities[a]] = StaxVehicle.New(entities[a])
+    end
+  elseif poolType == StaxPoolTypes.CPed then
+    for a = 1, #entities do
+      newPool.Pool[entities[a]] = StaxPed.New(entities[a])
+    end
+  elseif poolType == StaxPoolTypes.CPickup then
+    exports.stax_core:LogError("StaxGamePool couldn't be created", "[PoolType]: " .. tostring(poolType) .. " not supported yet.")
+    return nil
+  elseif poolType == StaxPoolTypes.CVehicle then
+    exports.stax_core:LogError("StaxGamePool couldn't be created", "[PoolType]: " .. tostring(poolType) .. " not supported yet.")
+    return nil
+  else
+    exports.stax_core:LogError("StaxGamePool couldn't be created", "[PoolType]: " .. tostring(poolType))
+    return nil
+  end
 
   return newPool
 end
 
-function StaxGamePool:Get()
+--- Gets an entity from the pool
+---@generic T
+---@param handle number The handle of the entity you are trying to get
+---@return T
+function StaxGamePool:Get(handle)
+  return self.Pool[handle]
+end
 
+--- Gets an entity from the pool
+---@generic T
+---@return T[]
+function StaxGamePool:GetAll()
+  return self.Pool
 end
