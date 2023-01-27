fa5438e4ff248c1bd8941d89b7d46dee8e224058
Xander Harrison <xander.harrison77@gmail.com>
2023-01-26 22:29:25 -0500

    Added global enums for these

---
+++ shared/enums/character/features.lua
@@ -0,0 +1,23 @@
+---@enum StaxCharacterFeatures
+StaxCharacterFeatures = {
+  nose_width = 0,
+  nose_peak_height = 1,
+  nose_peak_length = 2,
+  nose_bone_height = 3,
+  nose_peak_lowering = 4,
+  nose_bone_twist = 5,
+  eyebrow_height = 6,
+  eyebrow_forward = 7,
+  cheek_bone_height = 8,
+  cheek_bone_width = 9,
+  cheek_width = 10,
+  eye_openings = 11,
+  lip_thickness = 12,
+  jaw_bone_width = 13,
+  jaw_bone_length = 14,
+  chin_bone_lowering = 15,
+  chin_bone_length = 16,
+  chin_bone_width = 17,
+  chin_hole = 18,
+  neck_thickness = 19
+}
---
+++ shared/enums/character/props.lua
@@ -0,0 +1,8 @@
+---@enum StaxCharacterProps
+StaxCharacterProps = {
+  hats = 0,
+  glasses = 1,
+  ears = 2,
+  watches = 6,
+  bracelets = 7
+}
