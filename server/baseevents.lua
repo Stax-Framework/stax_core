5f3bdae4040cc97c9cb5c5b94ddfc1d1d9589fed
Xander Harrison <xander.harrison77@gmail.com>
2023-02-04 16:23:29 -0500

    Converted base events to new wrapper

--- server/baseevents.lua
+++ server/baseevents.lua
@@ -1,82 +1,85 @@
-AddEventHandler("playerDropped", function(reason --[[ string ]])
-  local src = source
-  local player = StaxPlayerManager:GetPlayer(src)
-  TriggerEvent("STAX::Core::Server::PlayerDropped", player, reason)
+StaxEvent.CreateEvent("playerDropped", function(reason)
+  local player = StaxPlayerManager:GetPlayer(source)
+  StaxEvent.Fire("STAX::Core::Server::PlayerDropped", player, reason)
 end)
 
-AddEventHandler("entityCreated", function(handle --[[ number ]])
-  TriggerEvent("STAX::Core::Server::EntityCreated", handle)
+StaxEvent.CreateEvent("entityCreated", function(handle)
+  StaxEvent.Fire("STAX::Core::Server::EntityCreated", handle)
 end)
 
-AddEventHandler("entityCreating", function(handle --[[ number ]])
-  TriggerEvent("STAX::Core::Server::EntityCreating", handle)
+StaxEvent.CreateEvent("entityCreating", function(handle)
+  StaxEvent.Fire("STAX::Core::Server::EntityCreating", handle)
 end)
 
-AddEventHandler("entityRemoved", function(handle --[[ number ]])
-  TriggerEvent("STAX::Core::Server::EntityRemoved", handle)
+StaxEvent.CreateEvent("entityRemoved", function(handle)
+  StaxEvent.Fire("STAX::Core::Server::EntityRemoved", handle)
 end)
 
-AddEventHandler("onResourceListRefresh", function()
-  TriggerEvent("STAX::Core::Server::OnResourceListRefresh")
+
+
+
+
+
+StaxEvent.CreateEvent("onResourceListRefresh", function()
+  StaxEvent.Fire("STAX::Core::Server::OnResourceListRefresh")
 end)
 
-AddEventHandler("onResourceStart", function(resource --[[ string ]])
-  TriggerEvent("STAX::Core::Server::OnResourceStart", resource)
+StaxEvent.CreateEvent("onResourceStart", function(resource)
+  StaxEvent.Fire("STAX::Core::Server::OnResourceStart", resource)
 end)
 
-AddEventHandler("onResourceStarting", function(resource --[[ string ]])
-  TriggerEvent("STAX::Core::Server::OnResourceStarting", resource)
+StaxEvent.CreateEvent("onResourceStarting", function(resource)
+  StaxEvent.Fire("STAX::Core::Server::OnResourceStarting", resource)
 end)
 
-AddEventHandler("onResourceStop", function(resource --[[ string ]])
-  TriggerEvent("STAX::Core::Server::OnResourceStop", resource)
+StaxEvent.CreateEvent("onResourceStop", function(resource)
+  StaxEvent.Fire("STAX::Core::Server::OnResourceStop", resource)
 end)
 
-AddEventHandler("onServerResourceStart", function(resource --[[ string ]])
-  TriggerEvent("STAX::Core::Server::OnServerResourceStart", resource)
+StaxEvent.CreateEvent("onServerResourceStart", function(resource)
+  StaxEvent.Fire("STAX::Core::Server::OnServerResourceStart", resource)
 end)
 
-AddEventHandler("onServerResourceStop", function(resource --[[ string ]])
-  TriggerEvent("STAX::Core::Server::OnServerResourceStop", resource)
+StaxEvent.CreateEvent("onServerResourceStop", function(resource)
+  StaxEvent.Fire("STAX::Core::Server::OnServerResourceStop", resource)
 end)
 
-AddEventHandler("playerConnecting", function(playerName --[[ string ]], setKickReason --[[ function ]], deferrals --[[ table ]])
-  local src = source
-  local player = StaxPlayer.New(src)
-  TriggerEvent("STAX::Core::Server::PlayerConnecting", player, deferrals)
+StaxEvent.CreateEvent("playerConnecting", function(name, setKick, deferrals)
+  local player = StaxPlayer.New(source)
+  StaxEvent.Fire("STAX::Core::Server::PlayerConnecting", player, deferrals)
 end)
 
-AddEventHandler("playerJoining", function(source --[[ number ]], oldSource --[[ number ]])
-  local src = source
-  local player = StaxPlayer.New(src)
-  TriggerEvent("STAX::Core::Server::PlayerJoining", src, oldSource, player)
+StaxEvent.CreateEvent("playerJoining", function(source, oldSource)
+  local player = StaxPlayer.New(source)
+  StaxEvent.Fire("STAX::Core::Server::PlayerJoining", source, oldSource, player)
 end)
 
-AddEventHandler("playerEnteredScope", function(scope --[[ table ]])
-  local src = source
-  local player = StaxPlayerManager:GetPlayer(src)
-  TriggerEvent("STAX::Core::Server::PlayerEnteredScope", player, scope)
+StaxEvent.CreateEvent("playerEnteredScope", function(scope)
+  local player = StaxPlayerManager:GetPlayer(source)
+  StaxEvent.Fire("STAX::Core::Server::PlayerEnteredScope", player, scope)
 end)
 
-AddEventHandler("playerLeftScope", function(scope --[[ table ]])
-  local src = source
-  local player = StaxPlayerManager:GetPlayer(src)
-  TriggerEvent("STAX::Core::Server::PlayerLeftScope", player, scope)
+StaxEvent.CreateEvent("playerLeftScope", function(scope)
+  local player = StaxPlayerManager:GetPlayer(source)
+  StaxEvent.Fire("STAX::Core::Server::PlayerLeftScope", player, scope)
 end)
 
-AddEventHandler("ptFxEvent", function(player --[[ number ]], data --[[ table ]])
-  TriggerEvent("STAX::Core::Server::PTFXEvent", player, data)
+StaxEvent.CreateEvent("ptFxEvent", function(playerHandle, data)
+  local player = StaxPlayerManager:GetPlayer(playerHandle)
+  StaxEvent.Fire("STAX::Core::Server::PTFXEvent", player, data)
 end)
 
-AddEventHandler("removeAllWeaponsEvent", function(player --[[ number ]], data --[[ table ]])
-  TriggerEvent("STAX::Core::Server::RemoveAllWeaponsEvent", player, data)
+StaxEvent.CreateEvent("removeAllWeaponsEvent", function(playerHandle, data)
+  local player = StaxPlayerManager:GetPlayer(playerHandle)
+  StaxEvent.Fire("STAX::Core::Server::RemoveAllWeaponsEvent", player, data)
 end)
 
-AddEventHandler("startProjectileEvent", function(player --[[ number ]], data --[[ table ]])
-  TriggerEvent("STAX::Core::Server::StartProjectileEvent", player, data)
+StaxEvent.CreateEvent("startProjectileEvent", function(playerHandle, data)
+  local player = StaxPlayerManager:GetPlayer(playerHandle)
+  StaxEvent.Fire("STAX::Core::Server::StartProjectileEvent", player)
 end)
 
-AddEventHandler("weaponDamageEvent", function(player --[[ number ]], data --[[ table ]])
-  TriggerEvent("STAX::Core::Server::WeaponDamageEvent", player, data)
+StaxEvent.CreateEvent("weaponDamageEvent", function(playerHandle, data)
+  local player = StaxPlayerManager:GetPlayer(playerHandle)
+  StaxEvent.Fire("STAX::Core::Server::WeaponDamageEvent", player, data)
 end)
-
