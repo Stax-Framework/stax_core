e12921bc999eab17ad9c9e0f689add90a3a20a26
Xander Harrison <xander.harrison77@gmail.com>
2023-02-08 09:28:58 -0500

    Enabled config script

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -18,7 +18,7 @@ shared_scripts {
   "@stax_fivem/stax_core/shared/classes/config.lua",
   "@stax_fivem/stax_core/shared/classes/locale.lua",
   "@stax_fivem/stax_core/server/classes/user.lua",
-  -- "shared/config.lua",
+  "shared/config.lua",
   "shared/locale.lua"
 }
 
