22e9f849264b18945c93cd2abf9a56a778f4ed4e
Xander Harrison <xander.harrison77@gmail.com>
2023-02-08 09:29:55 -0500

    Added config global for script

--- shared/config.lua
+++ shared/config.lua
@@ -1,14 +1,14 @@
 ---@type StaxConfig
-CoreConfig = {}
+Config = {}
 
 local isServer = IsDuplicityVersion()
 
 if isServer then
   StaxEvent.CreateEvent("STAX::Core::Shared::ConfigListener", function(config)
-    CoreConfig = StaxConfig.Class(config)
+    Config = StaxConfig.Class(config)
   end)
 else
   StaxEvent.CreateNetEvent("STAX::Core::Shared::ConfigListener", function(config)
-    CoreConfig = StaxConfig.Class(config)
+    Config = StaxConfig.Class(config)
   end)
 end
